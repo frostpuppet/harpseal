79d17e94e7d51afff45754b1e1a64e7c27f48db9
ssut <ssut@ssut.me>
2015-07-20 01:13:13 +0900

    Fix: typo

--- harpseal.py
+++ harpseal.py
@@ -6,25 +6,28 @@ harpseal
 """
 import argparse
 import asyncio
-import aiohttp
 import sys
 
+from harpseal.app import Harpseal
+
 def main():
-    prase = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description='Harpseal Command Line Applcation')
     args = parser.parse_args()
 
     app = Harpseal()
 
+    retcode = 0
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(app.start(loop))
     except KeyboardInterrupt:
         pass
-    except:
-        pass
+    except Exception as exc:
+        retcode = 1
+        print(exc)
 
-    return 0
+    return retcode
 
 if __name__ == '__main__':
     sys.exit(main())
