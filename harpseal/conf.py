b05d7fbbb2d552bb4c6231d0af34313160066006
ssut <ssut@ssut.me>
2015-07-21 22:56:14 +0900

    Add: `Config` class to access to the configuration file

--- harpseal/conf.py
+++ harpseal/conf.py
@@ -1,4 +1,21 @@
 """
 config parser
 """
+import os
+import json
 
+class Config(object):
+    def __init__(self, path):
+        if not os.path.exists(path):
+            raise IOError("The path of the config file does not exist.")
+        self._conf = {}
+        with open(path, 'r', encoding='utf-8') as f:
+            self._conf = json.loads(f.read())
+        if not self._conf:
+            raise IOError("Failed to load the config file or invalid JSON format.")
+
+    def __getitem__(self, key):
+        result = None
+        if key in self._conf:
+            result = self._conf[key]
+        return result
