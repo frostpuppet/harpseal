aabc6e353c454e9ee39f3a23f8d4dbdf267e67e4
ssut <ssut@ssut.me>
2015-07-22 01:49:16 +0900

    Fix: `PeriodicTask` works properly

--- harpseal/classes.py
+++ harpseal/classes.py
@@ -6,31 +6,31 @@
 """
 import asyncio
 
+__all__ = ['PeriodicTask']
+
 class PeriodicTask(object):
     """
     Make a periodic task
     """
 
-    def __init__(self, interval, func, app):
-        self._interval = interval
-        self._func = func
+    def __init__(self, plugin, app):
+        self.plugin = plugin
+        self._interval = plugin.properties['every']
+        self._func = plugin.execute
         self._app = app
+        self._task = None
 
+    @asyncio.coroutine
     def _run(self):
-        self.run()
-        self._handler = self._app.loop.call_later(self._interval, self._run)
+        while 1:
+            yield from asyncio.sleep(self._interval)
+            yield from self.run()
 
+    @asyncio.coroutine
     def run(self):
-        result = None
-        if self._func.__name__ == 'coroutine':
-            result = yield from self._func()
-        else:
-            result = self._func()
+        result = yield from self._func()
         if result is not None:
             yield from self._app.queue.put(result)
 
     def start(self):
-        self._handler = self._app.loop.call_later(self._interval, self._run)
-
-    def stop(self):
-        self._handler.cancel()
+        self._task = asyncio.Task(self._run())
