1b089141ca032d16ddd6f7f4b1c160498a396dfb
ssut <ssut@ssut.me>
2015-07-23 18:30:12 +0900

    Add: DiskPlugin

---
+++ harpseal/plugins/disk/__init__.py
@@ -0,0 +1,50 @@
+"""
+harpseal.plugins.disk
+~~~~~~~~~~~~~~~~~~~~
+
+"""
+import asyncio
+
+import psutil
+
+from harpseal.plugin import Plugin
+
+class DiskPlugin(Plugin):
+    name = 'disk'
+    description = 'disk watch plugin'
+    priority = 0
+    every = 5
+
+    def init(self):
+        self.partitions = partitions = psutil.disk_partitions()
+        for p in range(partitions):
+            name = '{}_usage'.format(p.device.split('/')[-1])
+            self.field_types[name] = 'stack'
+            self.fields[name] = [('used', int, ), ('free', int, )]
+
+        ios = psutil.disk_io_counters(perdisk=True)
+        for device in ios.keys():
+            name = '{}_io'.format(device)
+            self.field_type[device] = 'line'
+            for i in ['read_count', 'write_count', 'read_bytes', 'write_bytes',
+                      'read_time', 'write_time']:
+                self.fields[name].append(i)
+
+    @asyncio.coroutine
+    def provider(self):
+        data = self.data_form()
+
+        for p in range(partitions):
+            name = '{}_usage'.format(p.device.split('/')[-1])
+            usage = psutil.disk_usage(p.mountpoint)
+            data[name].set('used', usage.used)
+            data[name].set('free', usage.free)
+
+        ios = psutil.disk_io_counters(perdisk=True)
+        for device, values in ios.items():
+            name = '{}_io'.format(device)
+            for i in ['read_count', 'write_count', 'read_bytes', 'write_bytes',
+                      'read_time', 'write_time']:
+                data[name].set(i, getattr(values, i))
+
+        return data
