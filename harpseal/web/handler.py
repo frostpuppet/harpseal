219a0512265134059c000d0636443dd9582e72b3
ssut <ssut@ssut.me>
2015-07-24 23:27:24 +0900

    Add: a decorator that checks for proper plugin name

--- harpseal/web/handler.py
+++ harpseal/web/handler.py
@@ -7,11 +7,23 @@ import asyncio
 
 from harpseal.web import Response
 
+__all__ = ['Handler']
+
+def plugin_required(func):
+    def decorator(self, req):
+        name = req.match_info.get('name')
+        if name not in self.plugins.keys():
+            return Response({'ok': False, 'reason': 'Plugin does not exist.'})
+        return func(self, req)
+    return decorator
+
+
 class Handler(object):
     def __init__(self, plugins):
         self.plugins = {plugin.name: plugin for plugin in plugins}
 
     @asyncio.coroutine
+    @plugin_required
     def plugin_handler(self, req):
         name = req.match_info.get('name')
         return Response({'name': name})
