0d04a4f10a2309ae67184fcd250d05059e349654
ssut <ssut@ssut.me>
2015-07-25 00:39:20 +0900

    Add: a method that collects plugin logs

--- harpseal/web/handler.py
+++ harpseal/web/handler.py
@@ -4,6 +4,7 @@
 
 """
 import asyncio
+from datetime import datetime
 
 from harpseal.utils import datetime as dtutils
 from harpseal.web import Response
@@ -35,6 +36,31 @@ class Handler(object):
 
         return data
 
+    def get_plugin_logs(self, name, gte, lte=None):
+        if name not in self.plugins.keys():
+            raise KeyError("Plugin does not exist.")
+        if lte is None:
+            lte = datetime.now()
+
+        data = {}
+        plugin = self.plugins[name]
+        for k, v in plugin.fields.items():
+            fielddata = {
+                'type': plugin.field_types[k],
+                'legends': ['created'] + [n for n, _ in v],
+                'data': [],
+            }
+            records = plugin.models[k].objects(created_at__gte=gte,
+                                               created_at__lte=lte)
+            for record in records:
+                items = [dtutils.unixtime(record.created_at)]
+                for n, _ in v:
+                    items.append(getattr(record.items, n))
+                fielddata['data'].append(items)
+            data[k] = fielddata
+
+        return data
+
     @asyncio.coroutine
     def plugin_list_handler(self, req):
         return Response(self.get_plugin_list(withdetails=True))
@@ -43,4 +69,12 @@ class Handler(object):
     @plugin_required
     def plugin_handler(self, req):
         name = req.match_info.get('name')
-        return Response({'name': name})
+        plugin = self.get_plugin_list(withdetails=True)[name]
+        data = self.get_plugin_logs(name, gte=dtutils.ago(days=7))
+        data = {
+            'name': name,
+            'data': data,
+        }
+        data.update(plugin)
+
+        return Response(data)
