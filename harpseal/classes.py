feedc11c9399949f8ee432c898adac7efda1466a
ssut <ssut@ssut.me>
2015-07-28 01:37:17 +0900

    Add: Singleton implementation

--- harpseal/classes.py
+++ harpseal/classes.py
@@ -66,3 +66,13 @@ class StrictDict(object):
     def keys(self):
         """Return the keys of dict. The result is not a kind of `view`."""
         return self._keys
+
+
+class Singleton(type):
+    """Singleton pattern implementation."""
+
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
